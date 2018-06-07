47f1078346fbe791bd8bf7634af1f56ed9c86ef9
Kevin Matthiesen <kmatthiesen@catalyte.io>
2018-06-07 09:17:37 -0700

    updated gadd to all excluding of files based on project

--- bashcommands/gadd.sh
+++ bashcommands/gadd.sh
@@ -3,4 +3,12 @@ if [ "$1x" == "x" ]; then
 else
   git add $1;
 fi
+
+case $PWD in
+    */webcomponents) 
+        git reset js/packages/aem/package.json;
+        git reset js/packages/dev-portal/src/playground/home.html;;
+    *);;
+esac
+
 git status;
